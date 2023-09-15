ecb9f90e9558bea6427f65c3dd8b7c056ad0065c
Тимур <tupaev05@mail.ru>
2023-09-15 21:07:07 +0300

    реализовано:Сделать лог в консоль о том что сервер запущен и о том что сервер прекратил работу  Добавить в проект файл google.html;
    При запросе например localhost:2323 отправлять страницу google.html;
    
     Добавить в проект файл appsetting.json следующего вида:
    
    {
    "Port": 2323,
    "Address": "http://127.0.0.1"
    }
     сервер был в режиме прослушки, до тех пор пока вы не введете команду "stop";

--- Program.cs
+++ Program.cs
@@ -1,32 +1,51 @@
-﻿
+﻿using System;
 using System.IO;
 using System.Net;
 using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace hw
+{
+    public class AppSettings
+    {
+        public int Port { get; set; }
+        public string Address { get; set; }
+    }
 
-HttpListener server = new HttpListener();
-// установка адресов прослушки
-server.Prefixes.Add();
-server.Start(); // начинаем прослушивать входящие подключения
-
-
-// получаем контекст
-var context = await server.GetContextAsync();
-
-var response = context.Response;
-// отправляемый в ответ код htmlвозвращает
-
-StreamReader reader = new StreamReader("C:\\Users\\tupae\\OneDrive\\Рабочий стол\\ITISHW\\Google\\index.html");
-string responseText = await reader.ReadToEndAsync();
-//
-byte[] buffer = Encoding.UTF8.GetBytes(responseText);
-// получаем поток ответа и пишем в него ответ
-response.ContentLength64 = buffer.Length;
-using Stream output = response.OutputStream;
-// отправляем данные
-await output.WriteAsync(buffer);
-await output.FlushAsync();
-
-Console.WriteLine("Запрос обработан");
-
-server.Stop();
-Console.WriteLine("Server ended");
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string appSettingsPath = "appsetting.json";
+            string json = File.ReadAllText("C:\\Users\\tupae\\source\\repos\\MyHttpServer\\appsetting.json");
+            AppSettings settings = JsonConvert.DeserializeObject<AppSettings>(json);
+            string prefix = $"{settings.Address}:{settings.Port}/";
+            HttpListener server = new HttpListener();
+            server.Prefixes.Add(prefix);
+            server.Start();
+            Console.WriteLine($"Сервер запущен на {prefix}");
+            Console.WriteLine("Для остановки сервера введите '/stop' и нажмите Enter.");
+            while (true)
+            {
+                var context = await server.GetContextAsync();
+                var request = context.Request;
+                var response = context.Response;
+                if (request.HttpMethod == "GET" && request.Url.AbsolutePath == "/stop")
+                {
+                    Console.WriteLine("Получен запрос на остановку сервера.");
+                    server.Stop();
+                    Console.WriteLine("Сервер остановлен.");
+                    break;
+                }
+                StreamReader reader = new StreamReader("C:\\Users\\tupae\\OneDrive\\Рабочий стол\\ITISHW\\Google\\index.html");
+                string responseText = await reader.ReadToEndAsync();
+                byte[] buffer = Encoding.UTF8.GetBytes(responseText);
+                response.ContentLength64 = buffer.Length;
+                using Stream output = response.OutputStream;
+                await output.WriteAsync(buffer);
+                await output.FlushAsync();
+                Console.WriteLine("Запрос обработан");
+            }
+        }
+    }
+}
