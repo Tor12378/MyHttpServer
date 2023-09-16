fe462bcd1f12ee99d28627d876d9ff11125d52bf
Тимур <tupaev05@mail.ru>
2023-09-16 08:19:47 +0300

    Добавлен поток для чтения консоли

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+
 namespace hw
 {
     public class AppSettings
@@ -16,36 +17,49 @@ namespace hw
     {
         static async Task Main(string[] args)
         {
-            string appSettingsPath = "appsetting.json";
-            string json = File.ReadAllText("C:\\Users\\tupae\\source\\repos\\MyHttpServer\\appsetting.json");
+            string appSettingsPath = "C:\\Users\\tupae\\source\\repos\\MyHttpServer\\appsetting.json";
+            string json = File.ReadAllText(appSettingsPath);
             AppSettings settings = JsonConvert.DeserializeObject<AppSettings>(json);
             string prefix = $"{settings.Address}:{settings.Port}/";
             HttpListener server = new HttpListener();
             server.Prefixes.Add(prefix);
             server.Start();
             Console.WriteLine($"Сервер запущен на {prefix}");
-            Console.WriteLine("Для остановки сервера введите '/stop' и нажмите Enter.");
-            while (true)
+            Console.WriteLine("Для остановки сервера введите '/stop' в консоль и нажмите Enter.");
+
+            bool stopRequested = false;
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    string consoleInput = Console.ReadLine();
+                    if (consoleInput == "/stop")
+                    {
+                        Console.WriteLine("Получена команда на остановку сервера.");
+                        stopRequested = true;
+                        break;
+                    }
+                }
+            });
+
+            while (!stopRequested)
             {
                 var context = await server.GetContextAsync();
                 var request = context.Request;
                 var response = context.Response;
-                if (request.HttpMethod == "GET" && request.Url.AbsolutePath == "/stop")
-                {
-                    Console.WriteLine("Получен запрос на остановку сервера.");
-                    server.Stop();
-                    Console.WriteLine("Сервер остановлен.");
-                    break;
-                }
-                StreamReader reader = new StreamReader("C:\\Users\\tupae\\OneDrive\\Рабочий стол\\ITISHW\\Google\\index.html");
-                string responseText = await reader.ReadToEndAsync();
-                byte[] buffer = Encoding.UTF8.GetBytes(responseText);
+
+                string filePath = "C:\\Users\\tupae\\OneDrive\\Рабочий стол\\ITISHW\\Google\\index.html";
+                byte[] buffer = File.ReadAllBytes(filePath);
                 response.ContentLength64 = buffer.Length;
                 using Stream output = response.OutputStream;
                 await output.WriteAsync(buffer);
                 await output.FlushAsync();
                 Console.WriteLine("Запрос обработан");
             }
+
+            server.Stop();
+            Console.WriteLine("Сервер остановлен.");
         }
     }
 }
