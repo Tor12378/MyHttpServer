ad0572f59d6afb4a5e6c5d0caff4331efb5351e5
Тимур <tupaev05@mail.ru>
2023-09-20 09:04:23 +0300

    реализовано  Сделать отдельный класс для запуска остановки сервера. т.е. в programm должен быть только запуск вашего сервера

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace hw
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static void Main(string[] args)
         {
             AppSettings config = LoadServerConfig();
             if (config == null)
@@ -26,8 +26,7 @@ namespace hw
             Console.WriteLine($"Сервер запущен на {prefix}");
             Console.WriteLine("Для остановки сервера введите 'stop' в консоль и нажмите Enter.");
 
-            bool stopRequested = false;
-
+            var serverManager = new ServerManager(server,config);
             Task.Run(() =>
             {
                 while (true)
@@ -35,52 +34,15 @@ namespace hw
                     string consoleInput = Console.ReadLine();
                     if (consoleInput == "stop")
                     {
-                        Console.WriteLine("Получена команда на остановку сервера.");
-                        stopRequested = true;
-                        server.GetContext();
+                        serverManager.Stop();
                         break;
                     }
                 }
             });
 
-            while (!stopRequested)
-            {
-                var context = await server.GetContextAsync();
-                var request = context.Request;
-                var response = context.Response;
-
-                string requestUrl = request.Url.LocalPath;
-                if ( requestUrl.EndsWith(".html"))
-                {
-                    string filePath = Path.Combine(config.StaticFilesPath, requestUrl.TrimStart('/'));
-                    if (File.Exists(filePath))
-                    {
-                        byte[] buffer = File.ReadAllBytes(filePath);
-                        response.ContentLength64 = buffer.Length;
-                        using Stream output = response.OutputStream;
-                        await output.WriteAsync(buffer);
-                        await output.FlushAsync();
-                        Console.WriteLine($"Запрос обработан: {requestUrl}");
-                    }
-                    else
-                    {
-                        response.StatusCode = (int)HttpStatusCode.NotFound;
-                        response.ContentType = "text/plain; charset=utf-8";
-                        string notFoundMessage = "404 Файл не найден";
-                        byte[] notFoundBuffer = Encoding.UTF8.GetBytes(notFoundMessage);
-                        response.ContentLength64 = notFoundBuffer.Length;
-                        using Stream output = response.OutputStream;
-                        await output.WriteAsync(notFoundBuffer);
-                        await output.FlushAsync();
-                        Console.WriteLine($"Файл не найден: {requestUrl}");
-                    }
-                }
-
-            }
-
-            server.Stop();
-            Console.WriteLine("Сервер остановлен.");
+            serverManager.Start();
         }
+
         private static AppSettings LoadServerConfig()
         {
             string appSettingsPath = @".\appsettings.json";
@@ -97,6 +59,7 @@ namespace hw
                 return null;
             }
         }
+
         private static void EnsureStaticFilesPath(AppSettings config)
         {
             if (!Directory.Exists(config.StaticFilesPath))
@@ -112,7 +75,5 @@ namespace hw
                 }
             }
         }
-
     }
-
 }
---
+++ ServerManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using MyHttpServer;
+using Newtonsoft.Json;
+
+namespace hw
+{
+    public class ServerManager
+    {
+        private readonly HttpListener _server;
+        private readonly AppSettings _config;
+        private bool _stopRequested;
+
+        public ServerManager(HttpListener server, AppSettings config)
+        {
+            _server = server;
+            _config = config;
+            _stopRequested = false;
+        }
+
+        public void Start()
+        {
+            while (!_stopRequested)
+            {
+                try
+                {
+                    var context = _server.GetContext();
+                    var request = context.Request;
+                    var response = context.Response;
+
+                    string requestUrl = request.Url.LocalPath;
+                    if (requestUrl.EndsWith(".html"))
+                    {
+                        string filePath = Path.Combine(_config.StaticFilesPath, requestUrl.TrimStart('/'));
+                        if (File.Exists(filePath))
+                        {
+                            byte[] buffer = File.ReadAllBytes(filePath);
+                            response.ContentLength64 = buffer.Length;
+                            using Stream output = response.OutputStream;
+                            output.Write(buffer, 0, buffer.Length);
+                            output.Flush();
+                            Console.WriteLine($"Запрос обработан: {requestUrl}");
+                        }
+                        else
+                        {
+                            response.StatusCode = (int)HttpStatusCode.NotFound;
+                            response.ContentType = "text/plain; charset=utf-8";
+                            string notFoundMessage = "404 Файл не найден";
+                            byte[] notFoundBuffer = Encoding.UTF8.GetBytes(notFoundMessage);
+                            response.ContentLength64 = notFoundBuffer.Length;
+                            using Stream output = response.OutputStream;
+                            output.Write(notFoundBuffer, 0, notFoundBuffer.Length);
+                            output.Flush();
+                            Console.WriteLine($"Файл не найден: {requestUrl}");
+                        }
+                    }
+                    else
+                    {
+                        string filePath = Path.Combine(_config.StaticFilesPath, "Google.html");
+                        if (File.Exists(filePath))
+                        {
+                            byte[] buffer = File.ReadAllBytes(filePath);
+                            response.ContentLength64 = buffer.Length;
+                            using Stream output = response.OutputStream;
+                            output.Write(buffer, 0, buffer.Length);
+                            output.Flush();
+                            Console.WriteLine($"Запрос обработан: {requestUrl}");
+                        }
+                    }
+                }
+                catch (HttpListenerException ex) when (ex.ErrorCode == 995)
+                {
+                    // 995 - код ошибки, который указывает на остановку сервера
+                    break;
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("Получена команда на остановку сервера.");
+            _server.Close();
+            _stopRequested = true;
+            Console.WriteLine("Сервер остановлен.");
+        }
+    }
+}
